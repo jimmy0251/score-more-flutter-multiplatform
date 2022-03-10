7915ce5e9cb9b133dc45cfd232531606d8d5f936
jimmy0251 <jimmy0251@gmail.com>
2022-03-10 14:51:29 +0530

    Add database

--- lib/data/task.dart
+++ lib/data/task.dart
@@ -1,3 +1,6 @@
+
+import 'package:score_more/data/task_repository.dart';
+
 class Task {
   final String id;
   final String title;
@@ -6,4 +9,21 @@ class Task {
 
   Task(this.id, this.title, this.score, this.isComplete);
 
+  Map<String, Object?> toMap() {
+    var map = <String, Object?>{
+      columnTitle: title,
+      columnComplete: isComplete == true ? 1 : 0,
+      columnId: id,
+      columnScore: score
+    };
+    return map;
+  }
+
+  static Task fromMap(Map<String, Object?> map) {
+    final id = map[columnId].toString();
+    final title = map[columnTitle].toString();
+    final score = num.tryParse(map[columnScore].toString())?.toInt() ?? 0;
+    final isComplete = map[columnComplete] == 1;
+    return Task(id, title, score, isComplete);
+  }
 }
--- lib/data/task_repository.dart
+++ lib/data/task_repository.dart
@@ -1,52 +1,78 @@
-
 import 'dart:ffi';
 
 import 'package:score_more/data/task.dart';
+import 'package:sqflite/sqflite.dart';
+
+final String tableTodo = 'todo';
+
+final String columnId = '_id';
+final String columnTitle = 'title';
+final String columnComplete = 'complete';
+final String columnScore = 'score';
 
 class TaskRepository {
   static final TaskRepository INSTANCE = TaskRepository();
 
-  List<Task> tasks = [];
+  Database? _db;
 
-  Future<List<Task>> getTasks() {
-    return Future.value(tasks);
-  }
-
-  Future<bool> addTask(Task task) {
-    tasks.insert(0, task);
-    sortTasks();
-    return Future.value(true);
-  }
-
-  Future<bool> removeTask(Task task) {
-    tasks.remove(task);
-    sortTasks();
-    return Future.value(true);
-  }
-
-  Future<bool> updateTask(Task task) {
-    final index = tasks.indexWhere((element) => element.id == task.id);
-    if(index != -1) {
-      tasks[index] = task;
+  Future<Database> getDB() async {
+    final db = _db;
+    if(db != null) {
+      return db;
     }
-    sortTasks();
-    return Future.value(true);
-  }
-
-  void sortTasks() {
-    tasks.sort((a, b) {
-      if(a.isComplete == b.isComplete) {
-        return 0;
-      }
-
-      if(a.isComplete) {
-        return 1;
-      }
-      if(b.isComplete) {
-        return -1;
-      }
-     return 0;
+    return await openDatabase("todo.db", version: 1,
+        onCreate: (Database db, int version) async {
+      await db.execute('''create table $tableTodo (
+          $columnId text primary key, 
+          $columnTitle text not null,
+          $columnComplete integer not null,
+          $columnScore integer not null)
+          ''');
     });
   }
 
+  Future<List<Task>> getTasks() async {
+    final db = await getDB();
+    List<Map<String, Object?>> maps = await db.query(tableTodo,
+        columns: [columnId, columnComplete, columnTitle, columnScore]);
+
+    final tasks = maps.map((e) => Task.fromMap(e)).toList(growable: false);
+    sortTasks(tasks);
+    return tasks;
+  }
+
+  Future<bool> addTask(Task task) async {
+    final db = await getDB();
+    return db.insert(tableTodo, task.toMap()).then((value) => value != 0);
+  }
+
+  Future<bool> removeTask(Task task) async {
+    final db = await getDB();
+    return db.delete(tableTodo,
+        where: '$columnId = ?',
+        whereArgs: [task.id]).then((value) => value == 1);
+  }
+
+  Future<bool> updateTask(Task task) async {
+    final db = await getDB();
+    return db.update(tableTodo, task.toMap(),
+        where: '$columnId = ?',
+        whereArgs: [task.id]).then((value) => value == 1);
+  }
+
+  void sortTasks(List<Task> tasks) {
+    tasks.sort((a, b) {
+      if (a.isComplete == b.isComplete) {
+        return 0;
+      }
+
+      if (a.isComplete) {
+        return 1;
+      }
+      if (b.isComplete) {
+        return -1;
+      }
+      return 0;
+    });
+  }
 }
